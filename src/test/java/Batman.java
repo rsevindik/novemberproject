a2afd045d3297a5530737e0befe88082c58b22fc
rsevindik <rsuzem@gmail.com>
2020-11-01 11:23:54 -0500

    added comment2 on batman

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,6 +1,6 @@
 public class Batman {
     public static void main(String[] args) {
         System.out.println("Batman");
-        //more commentes
+        //more commentes Batman does not have power
     }
 }
